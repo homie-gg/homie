c22efa15f1162b84b652e186dfb3fa49af53d2ce
Mike Wu <hello@mikewu.dev>
2024-05-24 14:52:06 +0900

    add back NavBar

---
+++ src/app/_components/NavBar.tsx
@@ -0,0 +1,47 @@
+'use client'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from '@/lib/ui/NavigationMenu'
+
+import { buttonVariants } from '@/lib/ui/Button'
+// import { MobileMenu } from '@/app/_components/MobileMenu'
+import Link from 'next/link'
+
+export const Navbar = () => {
+  return (
+    <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
+      <NavigationMenu className="mx-auto overflow-hidden">
+        <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between">
+          <div className="flex justify-center items-center gap-8">
+            <NavigationMenuItem className="font-bold flex">
+              <a href="/" className="ml-2 font-black text-xl flex">
+                homie
+              </a>
+            </NavigationMenuItem>
+          </div>
+
+          <nav className="hidden md:flex gap-2">
+            <Link
+              href="/login"
+              className={`text-[0.875rem] ${buttonVariants({
+                variant: 'ghost',
+              })}`}
+            >
+              Login
+            </Link>
+            <Link
+              href="/sign_up"
+              className={`text-[0.875rem] ${buttonVariants({
+                variant: 'default',
+              })}`}
+            >
+              Try for free
+            </Link>
+          </nav>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  )
+}
