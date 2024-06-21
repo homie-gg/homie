030c982a5bd19b007d89cb5409d1ad723fd15c26
Mike Wu <hello@mikewu.dev>
2024-06-22 04:33:43 +0900

    fix(GitHub): webhooks getting fired multiple times

--- src/app/api/github/webhook/route.ts
+++ src/app/api/github/webhook/route.ts
@@ -1,14 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { dispatch } from '@/queue/default-queue'
-import { summaryKey } from '@/queue/handlers/handle-generate-open-pull-request-summary'
 import { logger } from '@/lib/log/logger'
-import { createGithubApp } from '@/lib/github/create-github-app'
-import { dbClient } from '@/database/client'
-import { assignContributorFromGithubIssue } from '@/lib/github/assign-contributor-from-github-issue'
-import { unassignContributorFromGithubIssue } from '@/lib/github/unassign-contributor-from-github-issue'
-import { closeTaskFromGithubIssue } from '@/lib/github/close-task-from-github-issue'
-import { deleteTaskFromGithubIssue } from '@/lib/github/delete-task-from-github-issue'
-import { reopenTaskFromGithubIssue } from '@/lib/github/reopen-task-from-github-issue'
+import { createGithubWebhooks } from '@/lib/github/create-github-webhooks'
 
 export const POST = async (request: NextRequest) => {
   logger.debug('Received Github webhook', {
@@ -16,112 +8,9 @@ export const POST = async (request: NextRequest) => {
     data: JSON.stringify(await request.clone().json()),
   })
 
-  const app = createGithubApp()
+  const githubWebhooks = createGithubWebhooks()
 
-  app.webhooks.on('issues.opened', async (params) => {
-    const {
-      payload: { issue, installation, repository },
-    } = params
-
-    await dispatch('create_homie_task_from_github_issue', {
-      issue,
-      repository,
-      installation,
-    })
-  })
-
-  app.webhooks.on('issues.assigned', async (params) => {
-    await assignContributorFromGithubIssue(params.payload)
-  })
-
-  app.webhooks.on('issues.unassigned', async (params) => {
-    await unassignContributorFromGithubIssue(params.payload)
-  })
-
-  app.webhooks.on('issues.closed', async (params) => {
-    await closeTaskFromGithubIssue(params.payload)
-  })
-
-  app.webhooks.on('issues.deleted', async (params) => {
-    await deleteTaskFromGithubIssue(params.payload)
-  })
-
-  app.webhooks.on('issues.edited', async (params) => {
-    const {
-      payload: { issue, installation, repository },
-    } = params
-
-    await dispatch('update_homie_task_from_github_issue', {
-      issue,
-      installation,
-      repository,
-    })
-  })
-
-  app.webhooks.on('issues.reopened', async (params) => {
-    await reopenTaskFromGithubIssue(params.payload)
-  })
-
-  app.webhooks.on('pull_request.closed', async (params) => {
-    const {
-      payload: { pull_request, installation },
-    } = params
-
-    await dispatch('save_merged_pull_request', {
-      pull_request,
-      installation,
-    })
-
-    const organization = await dbClient
-      .selectFrom('homie.organization')
-      .innerJoin(
-        'github.organization',
-        'github.organization.organization_id',
-        'homie.organization.id',
-      )
-
-      .where('ext_gh_install_id', '=', installation?.id!)
-      .select(['homie.organization.id'])
-      .executeTakeFirst()
-
-    if (!organization) {
-      return
-    }
-
-    await dispatch('close_linked_tasks', {
-      pullRequestBody: pull_request.body ?? '',
-      organization,
-    })
-  })
-
-  app.webhooks.on('pull_request.edited', async (params) => {
-    const { pull_request, installation } = params.payload
-
-    if (pull_request.body?.includes(summaryKey)) {
-      await dispatch('generate_open_pull_request_summary', {
-        pull_request,
-        installation,
-      })
-    }
-  })
-
-  app.webhooks.on('pull_request.opened', async (params) => {
-    const { pull_request, installation } = params.payload
-
-    await dispatch('save_opened_pull_request', {
-      pull_request,
-      installation,
-    })
-
-    if (pull_request.body?.includes(summaryKey)) {
-      await dispatch('generate_open_pull_request_summary', {
-        pull_request,
-        installation,
-      })
-    }
-  })
-
-  app.webhooks.verifyAndReceive({
+  await githubWebhooks.verifyAndReceive({
     id: request.headers.get('x-request-id')!,
     name: request.headers.get('x-github-event') as any,
     signature: request.headers.get('x-hub-signature')!,
---
+++ src/lib/github/create-github-webhooks.ts
@@ -0,0 +1,128 @@
+import { dispatch } from '@/queue/default-queue'
+import { summaryKey } from '@/queue/handlers/handle-generate-open-pull-request-summary'
+import { createGithubApp } from '@/lib/github/create-github-app'
+import { dbClient } from '@/database/client'
+import { assignContributorFromGithubIssue } from '@/lib/github/assign-contributor-from-github-issue'
+import { unassignContributorFromGithubIssue } from '@/lib/github/unassign-contributor-from-github-issue'
+import { closeTaskFromGithubIssue } from '@/lib/github/close-task-from-github-issue'
+import { deleteTaskFromGithubIssue } from '@/lib/github/delete-task-from-github-issue'
+import { reopenTaskFromGithubIssue } from '@/lib/github/reopen-task-from-github-issue'
+
+let webhooks: ReturnType<typeof createGithubApp>['webhooks'] | null = null
+
+export const createGithubWebhooks = () => {
+  if (webhooks) {
+    return webhooks
+  }
+
+  const app = createGithubApp()
+
+  app.webhooks.on('issues.opened', async (params) => {
+    const {
+      payload: { issue, installation, repository },
+    } = params
+
+    await dispatch('create_homie_task_from_github_issue', {
+      issue,
+      repository,
+      installation,
+    })
+  })
+
+  app.webhooks.on('issues.assigned', async (params) => {
+    await assignContributorFromGithubIssue(params.payload)
+  })
+
+  app.webhooks.on('issues.unassigned', async (params) => {
+    await unassignContributorFromGithubIssue(params.payload)
+  })
+
+  app.webhooks.on('issues.closed', async (params) => {
+    await closeTaskFromGithubIssue(params.payload)
+  })
+
+  app.webhooks.on('issues.deleted', async (params) => {
+    await deleteTaskFromGithubIssue(params.payload)
+  })
+
+  app.webhooks.on('issues.edited', async (params) => {
+    const {
+      payload: { issue, installation, repository },
+    } = params
+
+    await dispatch('update_homie_task_from_github_issue', {
+      issue,
+      installation,
+      repository,
+    })
+  })
+
+  app.webhooks.on('issues.reopened', async (params) => {
+    await reopenTaskFromGithubIssue(params.payload)
+  })
+
+  app.webhooks.on('pull_request.closed', async (params) => {
+    const {
+      payload: { pull_request, installation },
+    } = params
+
+    if (!pull_request.merged_at) {
+      return
+    }
+
+    await dispatch('save_merged_pull_request', {
+      pull_request,
+      installation,
+    })
+
+    const organization = await dbClient
+      .selectFrom('homie.organization')
+      .innerJoin(
+        'github.organization',
+        'github.organization.organization_id',
+        'homie.organization.id',
+      )
+
+      .where('ext_gh_install_id', '=', installation?.id!)
+      .select(['homie.organization.id'])
+      .executeTakeFirst()
+
+    if (!organization) {
+      return
+    }
+
+    await dispatch('close_linked_tasks', {
+      pullRequestBody: pull_request.body ?? '',
+      organization,
+    })
+  })
+
+  app.webhooks.on('pull_request.edited', async (params) => {
+    const { pull_request, installation } = params.payload
+
+    if (pull_request.body?.includes(summaryKey)) {
+      await dispatch('generate_open_pull_request_summary', {
+        pull_request,
+        installation,
+      })
+    }
+  })
+
+  app.webhooks.on('pull_request.opened', async (params) => {
+    const { pull_request, installation } = params.payload
+
+    await dispatch('save_opened_pull_request', {
+      pull_request,
+      installation,
+    })
+
+    if (pull_request.body?.includes(summaryKey)) {
+      await dispatch('generate_open_pull_request_summary', {
+        pull_request,
+        installation,
+      })
+    }
+  })
+
+  return app.webhooks
+}
