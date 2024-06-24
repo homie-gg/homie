444dc5352934c9718af4e89821cb22e7cd3c25da
Mike Wu <hello@mikewu.dev>
2024-06-25 00:58:54 +0900

    fix: add plan limit logging

--- src/lib/billing/get-is-over-plan-contributor-limit.ts
+++ src/lib/billing/get-is-over-plan-contributor-limit.ts
@@ -1,4 +1,6 @@
 import { dbClient } from '@/database/client'
+import { logger } from '@/lib/log/logger'
+import { getOrganizationLogData } from '@/lib/organization/get-organization-log-data'
 
 interface GetIsOverPlanContributorLimitParams {
   organization: {
@@ -12,6 +14,11 @@ export async function getIsOverPlanContributorLimit(
   params: GetIsOverPlanContributorLimitParams,
 ) {
   const { organization } = params
+
+  logger.debug('Checking over plan contributor limit', {
+    event: 'check_over_plan_contributor_limit:start',
+    organization: getOrganizationLogData(organization),
+  })
 
   const organizationWithBilling = await dbClient
     .selectFrom('homie.organization')
@@ -27,10 +34,23 @@ export async function getIsOverPlanContributorLimit(
       'homie.organization.id',
       'has_unlimited_usage',
       'homie.subscription.quantity as subscription_quantity',
+      'homie.plan.name as plan',
     ])
     .executeTakeFirst()
 
+  logger.debug('Got org billing info', {
+    event: 'check_over_plan_contributor_limit:got_org_with_billing',
+    organization: getOrganizationLogData(organization),
+    organization_with_billing: organizationWithBilling,
+  })
+
   if (organizationWithBilling?.has_unlimited_usage) {
+    logger.debug('Has unlimited usage', {
+      event: 'check_over_plan_contributor_limit:has_unlimited_usage',
+      organization: getOrganizationLogData(organization),
+      organization_with_billing: organizationWithBilling,
+    })
+
     return false
   }
 
@@ -42,8 +62,53 @@ export async function getIsOverPlanContributorLimit(
   const numContributors = contributors.length
 
   if (!organizationWithBilling?.subscription_quantity) {
-    return numContributors > freePlanContributorLimit
+    const isOverLimit = numContributors > freePlanContributorLimit
+
+    if (isOverLimit) {
+      logger.debug('Free plan - over limit', {
+        event: 'check_over_plan_contributor_limit:free_plan_over_limit',
+        organization: getOrganizationLogData(organization),
+        organization_with_billing: organizationWithBilling,
+        num_contributors: numContributors,
+        free_plan_limit: freePlanContributorLimit,
+      })
+
+      return true
+    }
+
+    logger.debug('Free plan - under limit', {
+      event: 'check_over_plan_contributor_limit:free_plan_under_limit',
+      organization: getOrganizationLogData(organization),
+      organization_with_billing: organizationWithBilling,
+      num_contributors: numContributors,
+      free_plan_limit: freePlanContributorLimit,
+    })
+
+    return false
   }
 
-  return numContributors > organizationWithBilling.subscription_quantity
+  const isOverLimit =
+    numContributors > organizationWithBilling.subscription_quantity
+
+  if (isOverLimit) {
+    logger.debug('Subscribed - over limit', {
+      event: 'check_over_plan_contributor_limit:subscribed_over_limit',
+      organization: getOrganizationLogData(organization),
+      organization_with_billing: organizationWithBilling,
+      num_contributors: numContributors,
+      num_seats_subscribed: organizationWithBilling.subscription_quantity,
+    })
+
+    return true
+  }
+
+  logger.debug('Subscribed - under limit', {
+    event: 'check_over_plan_contributor_limit:subscribed_under_limit',
+    organization: getOrganizationLogData(organization),
+    organization_with_billing: organizationWithBilling,
+    num_contributors: numContributors,
+    num_seats_subscribed: organizationWithBilling.subscription_quantity,
+  })
+
+  return false
 }
