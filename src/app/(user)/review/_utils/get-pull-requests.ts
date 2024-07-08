2b5b557d37cab1f1212cfacf046b53f8d89db021
Mike Wu <hello@mikewu.dev>
2024-07-09 02:12:53 +0700

    update to filte and query default branches

--- src/app/(user)/review/_utils/get-pull-requests.ts
+++ src/app/(user)/review/_utils/get-pull-requests.ts
@@ -24,36 +24,44 @@ export async function getPullRequests(
 ): Promise<PullRequest[]> {
   const { startDate, endDate, organization } = params
 
-  return dbClient
-    .selectFrom('homie.pull_request')
-    .where('homie.pull_request.created_at', '>=', startDate)
-    .where('homie.pull_request.created_at', '<=', endDate)
-    .where('homie.pull_request.organization_id', '=', organization.id)
-    .innerJoin(
-      'homie.contributor',
-      'homie.contributor.id',
-      'homie.pull_request.contributor_id',
-    )
-    .leftJoin(
-      'github.repo',
-      'github.repo.id',
-      'homie.pull_request.github_repo_id',
-    )
-    .leftJoin(
-      'gitlab.project',
-      'gitlab.project.id',
-      'homie.pull_request.gitlab_project_id',
-    )
-    .select([
-      'homie.pull_request.id',
-      'homie.pull_request.created_at',
-      'homie.pull_request.merged_at',
-      'homie.pull_request.closed_at',
-      'homie.contributor.username as user_username',
-      'homie.pull_request.title',
-      'homie.pull_request.contributor_id',
-      'github.repo.name as github_repo_name',
-      'gitlab.project.name as gitlab_project_name',
-    ])
-    .execute()
+  return (
+    dbClient
+      .selectFrom('homie.pull_request')
+      .where('homie.pull_request.created_at', '>=', startDate)
+      .where('homie.pull_request.created_at', '<=', endDate)
+      .where('homie.pull_request.organization_id', '=', organization.id)
+      // Only send PRs merged to default branch
+      .where((eb) =>
+        eb('was_merged_to_default_branch', '=', true)
+          // Assume no target_branch (legacy) to be default branch, which were the only PRs saved.
+          .or('target_branch', 'is', null),
+      )
+      .innerJoin(
+        'homie.contributor',
+        'homie.contributor.id',
+        'homie.pull_request.contributor_id',
+      )
+      .leftJoin(
+        'github.repo',
+        'github.repo.id',
+        'homie.pull_request.github_repo_id',
+      )
+      .leftJoin(
+        'gitlab.project',
+        'gitlab.project.id',
+        'homie.pull_request.gitlab_project_id',
+      )
+      .select([
+        'homie.pull_request.id',
+        'homie.pull_request.created_at',
+        'homie.pull_request.merged_at',
+        'homie.pull_request.closed_at',
+        'homie.contributor.username as user_username',
+        'homie.pull_request.title',
+        'homie.pull_request.contributor_id',
+        'github.repo.name as github_repo_name',
+        'gitlab.project.name as gitlab_project_name',
+      ])
+      .execute()
+  )
 }
