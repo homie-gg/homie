31fde2fd76489ba22015502429676b90891f0551
Mike Wu <hello@mikewu.dev>
2024-07-19 21:32:01 +0200

    update find task to use embedding

--- src/lib/tasks/find-task.ts
+++ src/lib/tasks/find-task.ts
@@ -1,4 +1,7 @@
 import { dbClient } from '@/database/client'
+import { createOpenAIEmbedder } from '@/lib/open-ai/create-open-ai-embedder'
+import { getPineconeClient } from '@/lib/pinecone/pinecone-client'
+import { CohereClient } from 'cohere-ai'
 
 interface FindTaskParams {
   organization_id: number
@@ -31,9 +34,54 @@ export async function findTask(
   }
 
   if (name) {
+    const query = `Task: ${name}`
+    const embedder = createOpenAIEmbedder({
+      modelName: 'text-embedding-3-large',
+    })
+    const embeddings = await embedder.embedQuery(query)
+    const index = getPineconeClient().Index(process.env.PINECONE_INDEX_MAIN!)
+
+    const { matches } = await index.query({
+      vector: embeddings,
+      topK: 10,
+      includeMetadata: true,
+      filter: {
+        organization_id: {
+          $eq: organization_id,
+        },
+        type: {
+          $eq: 'task',
+        },
+      },
+    })
+
+    const cohere = new CohereClient({
+      token: process.env.COHERE_API_KEY,
+    })
+
+    const reranked = await cohere.rerank({
+      query,
+      documents: matches.map((match) => (match.metadata?.text ?? '') as string),
+    })
+
+    if (reranked.results.length === 0) {
+      return null
+    }
+
+    const topResult = reranked.results[0]
+    const task_id = matches[topResult.index].metadata?.task_id
+
+    if (!task_id) {
+      return null
+    }
+
+    if (typeof task_id !== 'string') {
+      return null
+    }
+
     return dbClient
       .selectFrom('homie.task')
-      .where('homie.task.name', 'ilike', `%${name}%`)
+      .where('homie.task.id', '=', parseInt(task_id))
       .where('homie.task.organization_id', '=', organization_id)
       .select(['id', 'name', 'html_url', 'description'])
       .executeTakeFirst()
