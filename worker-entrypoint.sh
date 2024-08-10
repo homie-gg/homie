3869091bd7b0b85274d4fd7943abe77cf12c081d
Mike Wu <hello@mikewu.dev>
2024-08-11 02:07:26 +0900

    fix(Worker): running in watch mode

--- worker-entrypoint.sh
+++ worker-entrypoint.sh
@@ -3,8 +3,10 @@
 npm run queue:schedule-jobs
 npm run queue:dashboard &
 
-if [[ $WATCH_MODE -eq "true" ]]; then
+if [[ "$WATCH_MODE" == "true" ]]; then
+   echo "Running worker in watch mode"
    npx dotenv -e .env.local tsx -- --watch src/queue/work.ts
 else
+  echo "Running worker"
   npm run queue:work
 fi
