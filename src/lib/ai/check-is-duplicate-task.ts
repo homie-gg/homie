f3c686f5d36c3d7f175b2d36b52a2043bda6d470
Mike Wu <hello@mikewu.dev>
2024-08-11 02:18:20 +0900

    fix(Task/Duplicate): failing to check duplicate

--- src/lib/ai/check-is-duplicate-task.ts
+++ src/lib/ai/check-is-duplicate-task.ts
@@ -1,8 +1,7 @@
 import { logger } from '@/lib/log/logger'
-import { createOpenAIChatClient } from '@/lib/open-ai/create-open-ai-chat-client'
-import { StringOutputParser } from '@langchain/core/output_parsers'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
-import { RunnableSequence } from '@langchain/core/runnables'
+import OpenAI from 'openai'
+import { zodResponseFormat } from 'openai/helpers/zod.mjs'
+import { z } from 'zod'
 
 interface CheckIsTaskDuplicateParams {
   taskA: {
@@ -16,38 +15,57 @@ interface CheckIsTaskDuplicateParams {
   logData?: Record<string, any>
 }
 
+const checkIsDuplicateResponse = z.object({
+  is_duplicate: z.boolean().describe('Whether the two tasks are the same'),
+})
+
 export async function checkIsDuplicateTask(params: CheckIsTaskDuplicateParams) {
   const { taskA, taskB, logData } = params
 
-  const prompt = `There are two tasks, TASK A and TASK B. Determine if the two tasks are wanting to do the same thing. Answer must be TRUE/FALSE only.
+  const openAI = new OpenAI()
+
+  const result = await openAI.beta.chat.completions.parse({
+    model: 'gpt-4o-2024-08-06',
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a helpful project manager.',
+      },
+      {
+        role: 'user',
+        content: `There are two tasks, TASK A and TASK B. Determine if the two tasks are wanting to do the same thing. Answer must be TRUE/FALSE only.
 TASK A:
 ${taskA.name} - ${taskA.description}
 TASK B:
 ${taskB.name} - ${taskB.description}
-`
-
-  const chatPrompt = ChatPromptTemplate.fromTemplate(prompt)
-
-  const model = createOpenAIChatClient({
-    temperature: 0,
-    modelName: 'gpt-4o-2024-05-13',
+`,
+      },
+    ],
+    response_format: zodResponseFormat(
+      checkIsDuplicateResponse,
+      'checkIsDuplicateResponse',
+    ),
   })
-  const chain = RunnableSequence.from([
-    chatPrompt,
-    model,
-    new StringOutputParser(),
-  ])
+  const output = result.choices[0].message
+  if (!output?.parsed || output.refusal) {
+    logger.debug('Check duplicate task: failed to parse output', {
+      event: 'check_for_duplicate_task:failed_to_parse',
+      ai_call: true,
+      prompt,
+      output_refusal: output.refusal,
+      logData,
+    })
 
-  const answer = await chain.invoke({})
+    return false
+  }
 
-  logger.debug('AI: Check if duplicate task', {
-    event: 'check_for_duplicate_task:ask_ai',
+  logger.debug('Check duplicate task: got result', {
+    event: 'check_for_duplicate_task:failed_to_parse',
     ai_call: true,
     prompt,
-    answer,
-    is_duplicate: answer === 'TRUE',
     logData,
+    is_duplicate: output.parsed.is_duplicate,
   })
 
-  return answer === 'TRUE'
+  return output.parsed.is_duplicate
 }
