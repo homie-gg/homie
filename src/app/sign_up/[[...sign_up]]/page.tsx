ed166f982d294e32caa9c5c3663d7789146d0893
Mike Wu <hello@mikewu.dev>
2024-08-18 23:41:59 +0900

    feat(Analytics): add gtag sign up event

--- src/app/sign_up/[[...sign_up]]/page.tsx
+++ src/app/sign_up/[[...sign_up]]/page.tsx
@@ -1,9 +1,17 @@
 import { SignUp } from '@clerk/nextjs'
+import Script from 'next/script'
 
 export default function Page() {
   return (
-    <div className="flex justify-center py-24">
-      <SignUp />
-    </div>
+    <>
+      <Script id="google-analytics-sign-up">
+        {`
+            gtag('event', 'Sign_Up_Page');
+        `}
+      </Script>
+      <div className="flex justify-center py-24">
+        <SignUp />
+      </div>
+    </>
   )
 }
