b71c5a8b0114c5864b15a3fbc03dae8befbba837
Mike Wu <hello@mikewu.dev>
2024-08-19 11:53:54 +0900

    fix(Mailchimp): error on existing user

--- src/lib/mailchimp/subscribe-user.ts
+++ src/lib/mailchimp/subscribe-user.ts
@@ -1,4 +1,5 @@
 import { getMailchimp } from '@/lib/mailchimp/get-mailchimp'
+import { getMailchimpErrorResponse } from '@/lib/mailchimp/get-mailchimp-error-response'
 import { tags } from '@/lib/mailchimp/tags'
 
 interface SubscribeMailchimpUserParams {
@@ -8,19 +9,32 @@ interface SubscribeMailchimpUserParams {
 export async function subscribeUser(params: SubscribeMailchimpUserParams) {
   const { email } = params
 
-  const response = await getMailchimp().lists.addListMember(
-    process.env.MAILCHIMP_LIST_ID!,
-    {
-      status: 'subscribed',
-      email_address: email,
-      tags: [tags.signedUp],
-    },
-  )
+  try {
+    const response = await getMailchimp().lists.addListMember(
+      process.env.MAILCHIMP_LIST_ID!,
+      {
+        status: 'subscribed',
+        email_address: email,
+        tags: [tags.signedUp],
+      },
+    )
 
-  // Success
-  if ('contact_id' in response) {
-    return response.id
+    // Success
+    if ('contact_id' in response) {
+      return response.id
+    }
+
+    throw new Error('Failed to create Mailchimp contact.')
+  } catch (error: unknown) {
+    const errorResponse = getMailchimpErrorResponse(error)
+    if (!errorResponse) {
+      throw error
+    }
+
+    if (errorResponse.title === 'Member Exists') {
+      return
+    }
+
+    throw error
   }
-
-  throw new Error('Failed to create Mailchimp contact.')
 }
