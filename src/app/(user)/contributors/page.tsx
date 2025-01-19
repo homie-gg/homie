1310420a39fc8e79386eb51c79713f84a623e24f
Mike Wu <hello@mikewu.dev>
2025-01-19 11:34:53 +0900

    remove unused vars

--- src/app/(user)/contributors/page.tsx
+++ src/app/(user)/contributors/page.tsx
@@ -43,7 +43,7 @@ export default async function ContributorsPage(props: ContributorsPageProps) {
   }
   const slackClient = createSlackClient(organization.slack_access_token)
 
-  const { members: slackMembers = [], ok } = await getSlackUsers({
+  const { members: slackMembers = [] } = await getSlackUsers({
     slackClient,
   })
 
